90b3083cd4b7a5364f4f2c70b5885e154fb1c627
mahoshi naito <tatumnmix@gmail.com>
2022-10-17 20:57:32 +0900

    add Tablet ver.

--- cutCassis_tablet.js
+++ cutCassis_tablet.js
@@ -9,7 +9,9 @@ var topImg;
 var backImgs = [];
 var frameImg;
 
-var pKeyIsPressed = false;
+var cutting = false;
+
+var tips = [];
 
 function preload(){
   topImg = loadImage('./image/cassis.jpg');
@@ -20,15 +22,15 @@ function preload(){
   frameImg = loadImage('./image/frame.png');
 }
 function setup(){
-  wImg = Math.floor(topImg.width / 5);
-  hImg = Math.floor(topImg.height / 5);
-  
+  wImg = Math.floor(innerWidth * 0.9);
+  hImg = Math.floor(wImg * float(topImg.height / topImg.width))
+
   for(var p = 0; p < frameNum; p++){
     backImgs[p].resize(wImg, hImg);
   }
   topImg.resize(wImg, hImg);
   frameImg.resize(wImg,hImg);
-  createCanvas(window.outerWidth, window.outerHeight);
+  createCanvas(innerWidth, innerHeight);
   noStroke();
   smooth();
 
@@ -36,50 +38,52 @@ function setup(){
   pg.noStroke();
   pgFrame = createGraphics(wImg, hImg);
   pgFrame.noStroke();
-
 }
+
 function draw(){
-  // background(255,10,110);
-  image(backImgs[pFrame], (width - wImg) / 2, 30);
+  image(backImgs[pFrame], (innerWidth - wImg) / 2, (innerHeight - hImg) / 2);
   pg.background(topImg);
-  if(keyIsPressed && !pKeyIsPressed){
-    Cut(true, false);
-  }
-  else if(keyIsPressed && pKeyIsPressed){
-    Cut(false, false);
-  }
-  else if(!keyIsPressed && pKeyIsPressed){
-      Cut(false, true);
-  }
+  
   if(cutMask.length > 0){
     for(var p = 1; p < cutMask.length; p++){
       cutMask[p].displayBlood();
     }
     for(var p = 1; p < cutMask.length; p++){
       cutMask[p].display();
+      if(tips[p] == "end"){
+      
+      }
     }
   }
-  
-  // pgFrame.fill(255,10,110);
-  // pgFrame.rect(0, 0, wImg, hImg)
-  // pgFrame.erase();
-  // pgFrame.rect(20,20,wImg - 40, hImg - 40);
-  // pgFrame.noErase();
-  
-  image(pg,(width - wImg) / 2, 30);
-  
-  // image(frameImg, (width - wImg) / 2, 30);
-  // image(pgFrame,(width - wImg) / 2, 30);
-  
-  pKeyIsPressed = keyIsPressed;
+  image(pg, (innerWidth - wImg) / 2, (innerHeight - hImg) / 2);
+
   pFrame = (pFrame + 1) % frameNum;
+
+  // console.log(cutting)
+}
+
+function touchMoved(){
+  // ellipse(touches[0].x, touches[0].y, 50,50);
+  Cut(false, false);
+}
+
+function touchStarted(){
+  Cut(true, false);
+  // cutting = true;
+}
+
+function touchEnded(){
+  Cut(false, true);
+  // cutting = false;
 }
 
 function Cut(start, end){
   // 切り傷の太さ
-  let s = Math.round(randMinMax(5, 10));
-  // 切り傷作成
+  let s = Math.round(randMinMax(10, 30));
+
   cutMask.push(new mask(mX(mouseX), mY(mouseY), s, Math.round(randMinMax(50,500))));
+
+  tips.push( (start || end ) ? (start ? "start" : "end") : "");
   
   //補完
   var distX = mouseX - pmouseX;
@@ -90,22 +94,22 @@ function Cut(start, end){
   var dY = distY / plotNum;
 
   for(var p = 1; p <= plotNum; p++){
-    cutMask.push(new mask(mX(pmouseX) + p * dX, mY(pmouseY) + p * dY, Math.round(randMinMax(5, 10)), 0));
-    // print(pmouseX + p * dX, pmouseY + p * dY, cutSize(), 0)
+    cutMask.push(new mask(mX(mouseX) + p * dX, mY(mouseY) + p * dY, s, 0));
   }
 
-  //先端の処理
-  if(start){
-    for(var p = 1; p <= 100; p++){
-      cutMask.push(new mask(mX(mouseX) - p * dX, mY(mouseY) - p * dY, s * Math.pow(0.99, p),0 ));
-    }
-    
-  }
-  else if(end){
-    for(var p = 1; p <= 100; p++){
-      cutMask.push(new mask(mX(mouseX) + p * dX, mY(mouseY) + p * dY, s * Math.pow(0.99, p),0 ));
-    }
-  }
+  // //先端の処理
+  // if(start){
+  //   for(var p = 1; p <= 100; p++){
+  //     cutMask.push(new mask(mX(mouseX) - p * dX, mY(mouseY) - p * dY, s * Math.pow(0.99, p),0 ));
+  //   }
+  //   console.log("cut start");
+  // }
+  // else if(end){
+  //   for(var p = 1; p <= 100; p++){
+  //     cutMask.push(new mask(mX(mouseX) + p * dX, mY(mouseY) + p * dY, s * Math.pow(0.99, p),0 ));
+  //   }
+  //   console.log("cut end");
+  // }
 
 }
 
@@ -135,7 +139,7 @@ const mask = class{
   }
   //血液
   displayBlood(){
-    pg.fill(255,10,110,90);
+    pg.fill(255,10,110+randMinMax(-15,15),90 + randMinMax(-10, 100));
     pg.ellipse(this.x, this.y, this.s * randMinMax(1.3, 1.9));
     if(this.lifeTime % this.dropFreq == 0){
       this.dropBloods.push(new blood(this.x, this.y, this.s));
@@ -166,21 +170,22 @@ const blood = class{
     this.g = 9.8;
     this.v = 0;
     this.t = 0;
+    this.sMax = s * randMinMax(0.5, 1.5);
   }
   
   display(){
     pg.fill(255,10,110,90);
-    pg.ellipse(this.x, this.y, this.s * randMinMax(1.2, 1.5));
+    pg.ellipse(this.x, this.y, this.s * randMinMax(0.8, 1.2), this.s * randMinMax(1.2, 1.5));
     this.t++;
   }
 
   drop(){
-    if(this.t < 50){
+    if(this.s < this.sMax){
       this.s *= 1.01;
     }
     else{
       this.y += this.v;
-      this.v = 0.05 * this.g * this.t;
+      this.v = 0.01 * this.g * this.t;
     }
   }
 }
@@ -196,3 +201,10 @@ function mX(x) {
 function mY(y) {
   return y - 30;
 }
+
+function disableScroll(event) {
+  event.preventDefault();
+}
+
+// イベントと関数を紐付け
+document.addEventListener('touchmove', disableScroll, { passive: false });
